8a18cc7ad92927cf437ed7709ef76ec58a6ab849
Hebert Moraes <70861445+HebertMoraes@users.noreply.github.com>
2022-05-30 20:01:34 -0300

    Automação do ajuste tempo-narracao
    Primeira versão da automação do ajuste tempo-narracao, é feito por meio de arrays mais ainda falta automaticar com um laço de repetição na parte de texto.

--- Assets/Scripts/ChangeText.cs
+++ Assets/Scripts/ChangeText.cs
@@ -7,20 +7,41 @@ using TMPro;
 public class ChangeText : MonoBehaviour
 {
     public CinemachineVirtualCamera vcamObj;
+
+    public float[] positionsToChangeText;
+
+    public string[] textsOfNarration;
+
     private CinemachineTrackedDolly vcamComponent;
+    private int currentIndexNarration;
 
     // Start is called before the first frame update
     void Start()
     {
         vcamComponent = vcamObj.GetCinemachineComponent<CinemachineTrackedDolly>();
+        currentIndexNarration = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (vcamComponent.m_PathPosition >= 1) {
-            
-            gameObject.GetComponent<TextMeshProUGUI>().text = "aaaa troquei porra";
+        int i = 0;
+        foreach (float pos in positionsToChangeText) {
+            if (vcamComponent.m_PathPosition >= pos) {
+                //talvez precise adicionar +1 abaixo
+                currentIndexNarration = i;
+            }
+            i++;
+        }
+
+        if (currentIndexNarration == 0) {
+            gameObject.GetComponent<TextMeshProUGUI>().text = textsOfNarration[0];
+        }
+        if (currentIndexNarration == 1) {
+            gameObject.GetComponent<TextMeshProUGUI>().text = textsOfNarration[1];
+        }
+        if (currentIndexNarration == 2) {
+            gameObject.GetComponent<TextMeshProUGUI>().text = textsOfNarration[2];
         }
     }
 }
