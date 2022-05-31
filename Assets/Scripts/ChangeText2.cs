1d7fd663a391a510f095395ad9d49734e20f2062
Hebert Moraes <70861445+HebertMoraes@users.noreply.github.com>
2022-05-31 18:31:44 -0300

    finalizado o percurso, sem texto
    .

---
+++ Assets/Scripts/ChangeText2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeText2 : MonoBehaviour
+{
+    public float[] timeToChangeText;
+    public string[] textsOfNarration;
+    private float currentIndexNarration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
